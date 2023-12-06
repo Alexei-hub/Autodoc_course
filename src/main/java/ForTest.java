cd3b3c5d0f1073576e5365feb258d01e9d4675bc
Mushkambarian <alexeym@pkwteile.de>
2023-12-06 19:51:00 +0200

    feat: lesson 29

--- src/main/java/ForTest.java
+++ src/main/java/ForTest.java
@@ -1,3 +1,6 @@
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,28 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import static com.codeborne.selenide.Selenide.$$;
 import static java.lang.Thread.sleep;
 
 public class ForTest {
 
-    @Test
-    public void openTab() throws InterruptedException {
-        WebDriver driver  = new ChromeDriver();
-        driver.get("https://www.google.com.ua/");
-        sleep(5000);
-        driver.switchTo().newWindow(WindowType.TAB);
-        sleep(5000);
+    public static void main(String[] args) {
 
-        driver.switchTo().newWindow(WindowType.WINDOW);
-        sleep(5000);
-        Set<String> handles = driver.getWindowHandles();
-        System.out.println(handles.size());
-
-        List<String> test = new ArrayList<>(handles);
-        driver.switchTo().window(test.get(0));
-        sleep(5000);
-
-        driver.quit();
     }
 }
 
